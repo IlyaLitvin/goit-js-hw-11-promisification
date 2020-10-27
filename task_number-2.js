255d7569305b22ab07c07949e41f44da88546cad
IlyaLitvin <darnon88@gmail.com>
2020-10-27 21:29:25 +0200

    done

--- task_number-2.js
+++ task_number-2.js
@@ -5,24 +5,18 @@ const users = [
   { name: "Lux", active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
+const toggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map((user) =>
     user.name === userName ? { ...user, active: !user.active } : user
   );
-
-  callback(updatedUsers);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(updatedUsers);
+    }, 1000);
+  });
 };
 
 const logger = (updatedUsers) => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, "Mango", logger);
-toggleUserState(users, "Lux", logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, "Mango").then(logger);
 toggleUserState(users, "Lux").then(logger);
